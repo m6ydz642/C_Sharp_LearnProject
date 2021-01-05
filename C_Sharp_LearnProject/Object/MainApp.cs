dcbe65d182e4d8d7168b39ca7c68446660679270
m6ydz642 <m6ydz642@gmail.com>
2021-01-05 23:59:18 +0900

    boolean 건너뛰고 object로 바로 넘어감

---
+++ C_Sharp_LearnProject/Object/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_LearnProject.Overflow
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // 3.4.12 object형식
+
+            Console.WriteLine($"1>0 && 4<5 : {1 > 0 && 4 < 5}");
+
+
+        }
+    }
+}
---
+++ C_Sharp_LearnProject/test/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_LearnProject.Overflow
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine($"1>0 && 4<5 : {1 > 0 && 4 < 5}");
+
+
+        }
+    }
+}
