49ad980742d084c9c883cb08bc549856f1b628c4
m6ydz642 <m6ydz642@gmail.com>
2021-01-06 21:06:52 +0900

    언박싱 테스트

--- C_Sharp_LearnProject/BoxingUnboxing/MainApp.cs
+++ C_Sharp_LearnProject/BoxingUnboxing/MainApp.cs
@@ -9,9 +9,18 @@ namespace C_Sharp_LearnProject.Overflow
         static void Main(string[] args)
         {
             int a = 123;
-            object b = (object)a;
-            int c = (int)b;
-            // 하는중
+            object b = (object)a; // a의 담긴 값을 박싱해서 힙에 저장
+            int c = (int)b; // b에 담긴 값을 언박싱해서 스택에 저장
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            double x = 3.1414213;
+            object y = x;
+            double z = (double)y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
         }
     }
 }
