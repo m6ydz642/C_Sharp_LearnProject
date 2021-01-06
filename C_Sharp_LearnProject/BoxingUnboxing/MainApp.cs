14660576c033f209954b6e9435d1b9c4dfd2fb7c
m6ydz642 <m6ydz642@gmail.com>
2021-01-06 20:49:24 +0900

    박싱 언박싱 하는중 (컴퓨터 변경으로 인해 임시 커밋)

--- C_Sharp_LearnProject/BoxingUnboxing/MainApp.cs
+++ C_Sharp_LearnProject/BoxingUnboxing/MainApp.cs
@@ -5,14 +5,13 @@ using System.Text;
 namespace C_Sharp_LearnProject.Overflow
 {
     class MainApp
-    {
+    { //3.1.13 박싱과 언박싱
         static void Main(string[] args)
         {
-            object a = 123;
-            object b = "안녕하세요";
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
+            int a = 123;
+            object b = (object)a;
+            int c = (int)b;
+            // 하는중
         }
     }
 }
