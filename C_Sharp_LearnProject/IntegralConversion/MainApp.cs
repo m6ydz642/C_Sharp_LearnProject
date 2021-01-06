2fb0bc7ce84d2e5f4b3e0221d0df0e5c1e01a628
m6ydz642 <m6ydz642@gmail.com>
2021-01-06 22:03:19 +0900

    * MainApp.cs:
    * C_Sharp_LearnProject.csproj: 작업 환경 변경후 커밋, 크기가 서로 다른 정수 형식 사이의 변환

---
+++ C_Sharp_LearnProject/IntegralConversion/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_LearnProject.Overflow
+{
+    class MainApp
+    { //3.1.15 // 크기가 서로 다른 정수 형식 사이의 변환
+        static void Main(string[] args)
+        {
+            sbyte a = 127;
+            Console.WriteLine(a);
+
+            int b = (int)a;
+            Console.WriteLine(b);
+
+            int x = 128; // sbyte의 최대값 127보다 1큰 수
+            Console.WriteLine(x);
+
+            sbyte y = (sbyte)x;
+            Console.WriteLine(y);
+
+        }
+    }
+}
