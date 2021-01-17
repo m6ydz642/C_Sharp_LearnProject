f31a3e154999f1bb7b89388a979fe9e98150fee2
m6ydz642 <m6ydz642@gmail.com>
2021-01-17 23:45:30 +0900

    * MainApp.cs: 4장 추가 (대충 알고있던거 생략 ) 예)증감연산자 등등 바로 null연산자로 넘어옴, 4장에   챕터인줄알았는데 부록이라 부록으로 수정
    * MainApp.cs:
    * MainApp.cs:
    
    * C_Sharp_LearnProject.csproj: C_Sharp_LearnProject/3장/4장_데이터를 가공하는
      연산자/AssignmentOperator/MainApp.cs

---
+++ C_Sharp_LearnProject/테스트_혼자해보기/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+    class MainApp
+    { //맞춤 서식 문자열
+        static void Main(string[] args)
+        {
+            String a = null;
+            Console.WriteLine("a : {0}", a );
+            // Console.WriteLine("Total : {0, -7, D}", 123);
+        }
+    }
+}
