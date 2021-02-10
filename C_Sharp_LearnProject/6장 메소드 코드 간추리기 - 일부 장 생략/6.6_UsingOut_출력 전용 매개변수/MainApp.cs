8d8d5290c1a6cad88315116fb030e913232e14a2
m6ydz642 <m6ydz642@gmail.com>
2021-02-10 23:41:21 +0900

    출력 전용 매개변수

---
+++ C_Sharp_LearnProject/6장 메소드 코드 간추리기 - 일부 장 생략/6.6_UsingOut_출력 전용 매개변수/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingOut
+{
+    class MainApp
+    { // 출력전용 매개변수
+      static void Divide(int a, int b, out int quotient, out int reainder)
+        {
+            quotient = a / b;
+            reainder = a % b;
+        }
+
+   
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 3;
+            // int c;
+            // int d;
+
+            Divide(a, b, out int c, out int d);
+
+            Console.WriteLine($"a = {a}, b = {b}, a/b = {c}, a % b = {d}");       
+
+        }
+    }
+}
