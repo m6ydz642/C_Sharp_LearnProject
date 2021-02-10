2ed000d5c459449c4d3d65c8800e26eab5dba046
m6ydz642 <m6ydz642@gmail.com>
2021-02-10 23:23:26 +0900

    메소드 결과 참조로 반환

---
+++ C_Sharp_LearnProject/6장 메소드 코드 간추리기 - 일부 장 생략/6.5_RefReturn_메소드의 결과를 참조로 반환하기/MainApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefReturn
+{
+
+    class Product
+    {
+        private int price = 100;
+        public ref int GetPrice()
+        {
+            return ref price;
+        }
+
+        public void PrintPrice()
+        {
+            Console.WriteLine($"Price : {price}");
+        }
+    }
+    class MainApp
+    {
+
+   
+        static void Main(string[] args)
+        {
+            Product carrot = new Product();
+            ref int ref_local_price = ref carrot.GetPrice();
+            int normal_local_price = carrot.GetPrice();
+
+            carrot.PrintPrice();
+
+            Console.WriteLine($"ref local price : {ref_local_price}");
+            Console.WriteLine($"normal_local_price : {normal_local_price}");
+
+            ref_local_price = 200;
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"normal_local_price : {normal_local_price}");
+                 
+        }
+    }
+}
