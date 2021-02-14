feb7d5b2200b7d664b0ab69ce32ca08abc231435
m6ydz642 <m6ydz642@gmail.com>
2021-02-14 16:21:10 +0900

    this 생성자, 접근한정자 (private,public)등등 학습

---
+++ C_Sharp_LearnProject/6장 메소드 코드 간추리기 - 일부 장 생략/7장 클래스/7.6.2 this()생성자/ThisConstructor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThisConstructor
+{
+    class MyClass
+    {
+        int a, b, c;
+        public MyClass()
+        {
+            this.a = 5425;
+            Console.WriteLine("MyClass()");
+        }
+
+        public MyClass(int b) : this()
+        {
+            this.b = b;
+            Console.WriteLine($"MyClass{b}");
+        }
+
+        public MyClass(int b, int c) : this(b)
+        {
+            this.c = c;
+            Console.WriteLine($"MyClass({b}, {c}");
+        }
+
+        public void PrintFields()
+        {
+            Console.WriteLine($"{a}, b:{b}, c:{c}");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MyClass a = new MyClass(); // a값만 초기화
+            a.PrintFields();// a값만 초기화해서 출력
+            Console.WriteLine();
+
+            MyClass b = new MyClass(1); // b값을 1로 초기화 하면서 this로 기본생성자로 호출
+            b.PrintFields();
+            Console.WriteLine();
+
+            MyClass c = new MyClass(10,20); // 인자 2개짜리 생성자 호출하면서 
+            // b포함 호출하고 b는 a가 들어가있는 기본생성자도 호출
+            c.PrintFields();
+            Console.WriteLine();
+
+        }
+    }
+
+    
+}
---
+++ C_Sharp_LearnProject/6장 메소드 코드 간추리기 - 일부 장 생략/7장 클래스/7.7 접근 한정자/AccessModifier.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifier
+{
+  class WaterHeater
+    {
+        protected int temperature;
+
+        public void Setemperature(int temperature)
+        {
+            if (temperature < -5 || temperature > 42)
+            {
+                throw new Exception("Out of temperature range");
+            }
+            this.temperature = temperature;
+        }
+
+        internal void TurnOnWater()
+        {
+            Console.WriteLine($"Turn on water : {temperature}");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                WaterHeater heater = new WaterHeater();
+                heater.Setemperature(20);
+                heater.TurnOnWater();
+
+                heater.Setemperature(-2);
+                heater.TurnOnWater();
+
+                heater.Setemperature(50); // 위의 Setemperature 에 if문에 조건에 해당되므로 예외발생
+                heater.TurnOnWater();
+            }catch(Exception e)
+            {
+                Console.WriteLine("e message : {0}", e.Message);
+            }
+
+        }
+    }
+
+    
+}
