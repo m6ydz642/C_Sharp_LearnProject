1b16383257d33d43ea1d888c19c17fa3155a875e
m6ydz642 <m6ydz642@gmail.com>
2021-02-15 23:05:34 +0900

    중첩 메소드 등 학습

---
+++ C_Sharp_LearnProject/7장 클래스/7.13 읽기전용 필드/ReadOnly.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadOnly
+{
+   class Configration
+    {
+        private readonly int max;
+        private readonly int min;
+
+        public Configration(int max, int min)
+        {
+            this.min = max;
+            this.max = min;
+        }
+
+        public void test(int NewMax)
+        {
+            max = NewMax; // 맴버변수의 max가 readonly라 생성자에서 값변경하는거 
+                          // 아니면 사용불가
+            // NewMax = max; // 값을 넣을수는 있음
+        }
+    }
+
+
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            Configration a = new Configration(10,100);
+
+        }
+    }
+
+    
+}
---
+++ C_Sharp_LearnProject/7장 클래스/7.14 중첩클래스/NestedClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NestedClass
+{
+    class Configurtation
+    {
+        List<ItemValue> listConfig = new List<ItemValue>();
+
+        public void SetConfig(string item, string value)
+        {
+            ItemValue iv = new ItemValue();
+            Configurtation test = new Configurtation() ;
+            iv.SetValue(test, item, value);
+        }
+
+        public string GetConfig (string item)
+        {
+            foreach (ItemValue test in listConfig) 
+            {
+                if (test.GetItem() == item)
+                {
+                    return test.GetValue();
+                }
+            }
+            return "";
+        }
+        private class ItemValue
+        {
+            private string item;
+            private string value;
+
+            public void SetValue(Configurtation config, string item, string value)
+            {
+                this.item = item;
+                this.value = value;
+
+                bool found = false ;
+                for (int i =0; i<config.listConfig.Count; i++)
+                {
+                    if (config.listConfig[i].item == item)
+                    {
+                        config.listConfig[i] = this;
+                        found = true;
+                        break;
+                     }
+                }
+                if (found == false)
+                {
+                    config.listConfig.Add(this);
+                }
+
+            }
+
+
+            public string GetItem()
+            {
+                return item;
+            }
+            public string GetValue()
+            {
+                return value;
+            }
+
+        }
+    }
+  
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            Configurtation a = new Configurtation();
+            a.SetConfig("v","1234");
+            Console.WriteLine($"{a.GetConfig("v")}");
+
+        }
+    }
+
+    
+}
