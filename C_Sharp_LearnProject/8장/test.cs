eea527ecd236c9da9f2b85e514be97e6eb9e781b
m6ydz642 <m6ydz642@gmail.com>
2021-02-16 21:25:02 +0900

    * C_Sharp_LearnProject.csproj:
    * test.cs: Hello World

--- C_Sharp_LearnProject/8장/test.cs
+++ C_Sharp_LearnProject/8장/test.cs
@@ -1,1 +1,19 @@
-ㅇㅋ
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+   
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+
+        }
+    }
+
+
+}
