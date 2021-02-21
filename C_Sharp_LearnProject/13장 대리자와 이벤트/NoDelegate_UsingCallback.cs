d0033e4b49297e2402a6c381e9de9fe0d730d386
m6ydz642 <m6ydz642@gmail.com>
2021-02-21 23:25:52 +0900

    Delegate없이 호출 사용, Array주소 확인

---
+++ C_Sharp_LearnProject/13장 대리자와 이벤트/NoDelegate_UsingCallback.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace UsingCallback
+{
+    class MainApp
+    {
+      
+
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        static int DecendComoare(int a, int b) // BubbleSort함수에서 호출해주는게 없어 함수 미사용
+        {
+            if (a < b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        static void BubbleSort(int[] DataSet)
+        {
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+
+            for (i = 0; i<DataSet.Length-1; i++)
+            {
+                for (j=0; j<DataSet.Length - (i+1); j++)
+                {
+                    if (AscendCompare(DataSet[j], DataSet[j+1]) > 0)
+                    // 오름차순 정렬 호출 (대리자 없이)
+                    // 내림차순을 할 경우 BubbleSort2를 만들어서 if문을 
+                    // DecendComoare로 다시 호출해야 함
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 2, 10 };
+            Console.WriteLine("오름차순 전");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("오름차순후");
+            BubbleSort(array);
+            for (int i=0; i<array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ C_Sharp_LearnProject/테스트/VoidArray_Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test
+{
+  
+    class MainApp
+    { // 대리자 사용하다가 배열은 주소 방식 헷깔려서 테스트 하러옴
+
+        static void testvalue(int[] a)
+        {
+
+            a[0] = 1234;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = { 10 };
+            Console.WriteLine("a = " + a[0]);
+            testvalue(a); 
+            Console.WriteLine("변경후  a값 " + a[0]); // 주소 달라서 바뀜
+        }
+    }
+}
