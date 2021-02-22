38c2e584c8383606c3e35b85d3b906f5c708b45b
m6ydz642 <m6ydz642@gmail.com>
2021-02-22 21:29:09 +0900

    * DuplicateCheck.cs:
    * C_Sharp_LearnProject.csproj: test array

---
+++ C_Sharp_LearnProject/테스트/DuplicateCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace DuplicateCheck
+{
+   
+    class MainApp
+    {
+        static void Rand()
+
+        {
+            Random random = new Random();
+            int rand = random.Next(1,10);
+            int[] array = new int[3];
+            for (int i=0; i<array.Length; i++)
+            {
+                array[i] = rand;
+                Console.WriteLine("rand : " + rand);
+            }
+            
+        }
+
+        static void Main(string[] args)
+        { // 랜덤함수 중복체크
+            Rand();
+        }
+    }
+}
