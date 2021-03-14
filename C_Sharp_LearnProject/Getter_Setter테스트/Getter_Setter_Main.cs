20534db958ec25c89491b0d065fe3cd0212887db
m6ydz642 <m6ydz642@gmail.com>
2021-03-14 15:43:37 +0900

    get set 프로퍼티 테스트

---
+++ C_Sharp_LearnProject/Getter_Setter테스트/Getter_Setter_Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Getter_Setter
+{
+   
+   public class Getter_Setter_Main
+    {
+
+
+        // main 값 다른객체 생성자로 보내기
+        public void getset_test()
+        {
+            Getter_Setter_Value a = new Getter_Setter_Value();
+            Console.WriteLine(a.Number()); // 함수형 호출
+            Console.WriteLine(a.NumberGetSet); // getset 호출
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Getter_Setter_Main a = new Getter_Setter_Main();
+            a.getset_test();
+
+
+        }
+    }
+}
---
+++ C_Sharp_LearnProject/Getter_Setter테스트/Getter_Setter_Value.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Getter_Setter
+{
+   
+   public class Getter_Setter_Value
+    {
+
+        public int NumberGetSet
+        {
+            get; set;
+        }
+
+        public Getter_Setter_Value()
+        {
+
+        }
+
+        public int Number()
+        {
+             NumberGetSet = 1234;
+            int number = NumberGetSet + 1234;
+            return number;
+        }
+
+
+    }
+}
