b3f5deae32d06a8c825919b2110ce9493422f8a5
m6ydz642 <m6ydz642@gmail.com>
2021-04-01 21:20:49 +0900

    포인터 테스트

---
+++ C_Sharp_LearnProject/테스트/PointerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace PointerTest
+{
+  
+    class PointerTest
+    {
+        
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int number = 27;
+                int* pointer = &number;
+
+                Console.WriteLine("printer number : " + number);
+                Console.WriteLine("printer number : " + *pointer);
+
+                *pointer = 22;
+                Console.WriteLine("printer number : " + *pointer);
+
+            }
+            
+
+        
+        }
+    }
+}
