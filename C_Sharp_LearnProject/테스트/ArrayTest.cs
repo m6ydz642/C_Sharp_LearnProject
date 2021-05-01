9f209395433a9bbc09666480bb2529750e331e8d
m6ydz642 <m6ydz642@gmail.com>
2021-05-01 12:39:19 +0900

    string -> char 변환테스트

--- C_Sharp_LearnProject/테스트/ArrayTest.cs
+++ C_Sharp_LearnProject/테스트/ArrayTest.cs
@@ -16,7 +16,10 @@ namespace ArrayTest
             string test = "1234";
             Console.WriteLine("test " + test[2]);
             char test2 = test[2];
-            Console.WriteLine("test[2] array : " + test[2]); // 자바에서는 안되는데 C#에서는 되네
+            Console.WriteLine("test[2] array : " + test2); // 자바에서는 안되는데 C#에서는 되네
+
+            char test3 = test[2];
+          //  Console.WriteLine("test3 : " + test3[0]); // 이건 안됨
         }
     }
 }
