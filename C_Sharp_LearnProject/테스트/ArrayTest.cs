a7e8f70068297e5e6a517592bc24c962592c03b9
m6ydz642 <m6ydz642@gmail.com>
2021-05-30 15:38:03 +0900

    람다식 테스트

--- C_Sharp_LearnProject/테스트/ArrayTest.cs
+++ C_Sharp_LearnProject/테스트/ArrayTest.cs
@@ -6,11 +6,7 @@ namespace ArrayTest
    
     class ArrayTest
     {
-        
-        // 중복검사 순차 검사해보기
-        // 1234
-        // 234
-        // 34 순으로 확인
+       
         static void Main(string[] args)
         {
             string test = "1234";
---
+++ C_Sharp_LearnProject/테스트/RambdaTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace RambdaTest
+{
+   
+    class RambdaTest
+    {
+       
+        static void Main(string[] args)
+        {
+            Action Noreturn_lambda = () =>
+            {
+                Console.WriteLine("리턴할거 없고 매개변수 없는 람다식");
+            };
+
+            Action<string> Noreturn_lambda_parameter = (good) =>
+            {
+                Console.WriteLine("리턴할거 없고 매개변수 있는 람다식 : " + good);
+            };
+
+            Func<string,string> Noreturn_lambda_parameter2 = (good) =>
+            {
+                Console.WriteLine("리턴할거 있고 매개변수 있는 람다식 : " + good);
+                return good;
+            };
+
+            Func<string> Noreturn_lambda_noparameter2 = () =>
+            {
+                Console.WriteLine("리턴할거 있고 매개변수 없는 람다식 : ");
+                return "리턴했슈";
+            };
+
+            Noreturn_lambda();
+            Noreturn_lambda_parameter("test");
+           string returnLambda =  Noreturn_lambda_parameter2("test2");
+           string returnLambda2 = Noreturn_lambda_noparameter2();
+        }
+    }
+}
