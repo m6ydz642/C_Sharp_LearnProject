4dd8cf73c123d33ad060db3de4a57538c3053b40
m6ydz642 <m6ydz642@gmail.com>
2021-06-08 23:33:10 +0900

    약수 소인수 분해

--- C_Sharp_Math_Paractice/중학교/2-1소인수 분해/Small_phosphorus_decomposition2.cs
+++ C_Sharp_Math_Paractice/중학교/2-1소인수 분해/Small_phosphorus_decomposition2.cs
@@ -47,8 +47,42 @@ namespace C_Sharp_Math_Paractice
             foreach(int a in numberList)
            {
                Console.WriteLine("리스트 버전 " + SoinSu_Number + "의 약수 " + a);
+           }
+
+           // 소인수 분해 + 약수 구하기
+           // 문제 7) (1) 2^3 X 5 약수 구하기 
+
+           numberList = new List<int>();
+          List<int> SoinsuNumberList = new List<int>();
+
+
+           numberList.Add(2);
+           numberList.Add(2);
+           numberList.Add(2);
+           numberList.Add(5);
+
+           int j = 0;
+           for (int i= 0; i<numberList.Count; i++) {
+               j++;
+               if (j < numberList.Count && numberList[i].Equals(numberList[j]) )
+               {
+                   SoinsuNumberList.Add(numberList[i]);
+                  
+               }
+               if (j < numberList.Count && !numberList[i].Equals(numberList[j])) 
+               // 리스트 내의 내용이 맞지 않는 시점이 오면 그 전단계 값을 비교함
+              
+               {
+                   if(numberList[i-1].Equals(numberList[i-2]))
+                        SoinsuNumberList.Add(numberList[i-1]);
+                   // 0,1 1,2 2,3 을 비교했는데 0,1 1,2 까지 맞고 2,3부터 안맞으면 역순으로 돌아가 2,1를 비교해 1,2번째중 2번째의 값을 마지막으로 판단해 넣음
+                   // 0,1 비교시 같으면 리스트에 0번째 수를 넣고 1,2를 비교하면 1번째 수를 리스트에 넣는데 마지막 2번째는 조건이 안맞아 넣을수가 없기 때문임   
+                   // 대신 이방법은 i가 1이나 2이상이 되기 전에 조건이 안맞으면 i값이 마이너스로 가서 예외로 빠짐 ㅋㅋ;
+               }
 
            }
+
+
 
        };
     
