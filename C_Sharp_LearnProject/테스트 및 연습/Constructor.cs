a9827ae742e2a9fddb801210a8323c309b551fa2
m6ydz642 <m6ydz642@gmail.com>
2021-07-20 02:23:07 +0900

    인자전달 테스트

---
+++ C_Sharp_LearnProject/테스트 및 연습/Constructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_LearnProject.테스트_및_연습
+{
+    class Constructor
+    {
+        string test;
+        public Constructor()
+        {
+
+        }
+
+        public Constructor(string test) : this()
+        {
+            this.test = test;
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("테스트");
+        }
+
+
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Constructor constructor = new Constructor("인자전달 테스트");
+            constructor.Test();
+        }
+    }
+
+}
