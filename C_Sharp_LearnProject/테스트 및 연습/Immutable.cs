d93399ee760eb175e6c2e66c9d565776fe86d623
m6ydz642 <m6ydz642@gmail.com>
2021-07-21 22:22:24 +0900

    string불변 테스트

---
+++ C_Sharp_LearnProject/테스트 및 연습/Immutable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_LearnProject.테스트_및_연습
+{
+    class Immutable
+    {
+        static void Main(string[] args)
+        {
+            // string 불변 테스트 
+            string a = "test";
+            Console.WriteLine("해쉬코드 1 string : " + a.GetHashCode());
+
+            string d = a;
+            d += "와우";
+            Console.WriteLine("해쉬코드 2 string : " + d.GetHashCode());
+
+            int b = 12;
+            Console.WriteLine("해쉬코드 3 int : " + b.GetHashCode());
+            int c = b;
+            c += 123;
+ 
+            Console.WriteLine("해쉬코드 4 int : " + c.GetHashCode());
+        }
+    }
+}
