8ffbd72e773b1bc4c4edf72923b72ed404f7623a
m6ydz642 <m6ydz642@gmail.com>
2021-07-21 22:02:40 +0900

    using 자원해제
    IDisposable
    사용

---
+++ C_Sharp_LearnProject/테스트 및 연습/Finalize.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Finalize
+{
+    class TestFinalize : IDisposable
+    {
+        public TestFinalize()
+        {
+
+            Console.WriteLine("생성자");
+
+        }
+
+        public void Fun()
+        {
+            Console.WriteLine("Fun 함수호출");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("TestFinalize 메모리 해제");
+
+        }
+
+        ~TestFinalize() // using문으로 자원을 관리해 소멸자 필요없음
+        {
+            Console.WriteLine("TestFinalize 소멸자");
+        }
+    }
+    class Finalize
+    {
+        static void Main(string[] args)
+        {
+            using (TestFinalize fin = new TestFinalize())
+            {
+                fin.Fun();
+            }
+
+        }
+    }
+}
