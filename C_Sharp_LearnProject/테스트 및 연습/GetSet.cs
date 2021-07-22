93df99e754d52991a55621fda3ba6d39d453b2ee
m6ydz642 <m6ydz642@gmail.com>
2021-07-22 19:53:25 +0900

    get set 프로퍼티 테스트

---
+++ C_Sharp_LearnProject/테스트 및 연습/GetSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_LearnProject.테스트_및_연습
+{
+    class GetSet
+    {
+        // get set 프로퍼티 
+        public string _something;
+        public string Something
+        {
+            get
+            {
+                return _something;
+            }
+            set
+            {
+                _something = value;
+            }
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            GetSet gs = new GetSet();
+            gs._something = "와우";
+            Console.WriteLine("내용 : " + gs._something);
+            Console.WriteLine("내용 : " + gs.Something);
+
+        }
+    }
+}
---
+++ C_Sharp_LearnProject/테스트 및 연습/StringBuilderTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_LearnProject.테스트_및_연습
+{
+    class StringBuilderTest
+    {
+        static void Main(string[] args)
+        {
+            // string을 사용하지 않고 Builder를 사용
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("와우 \r\n");
+            sb.Append("리스트같네 \r\n");
+            sb.Append("테스트 \r\n").ToString().Trim();
+            sb.Append("테스트2");
+            Console.WriteLine(sb);
+
+ 
+            string str1 = string.Format("Example2 : {0}, {1}", "BlockDMask", 3434);
+            Console.WriteLine("test : " + str1); // string  format 으로 출력
+
+
+
+
+
+        }
+    }
+}
